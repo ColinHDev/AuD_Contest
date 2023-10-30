8f18bbee047fbafe5f06d92d5cb8711fe47e83b3
Colin <colinheidfeld@gmail.com>
2023-10-30 04:29:57 +0100

    Klassen für die Prozesskommunikation über Javas RMI hinzugefügt

--- core/src/com/example/networking/rmi/ProcessCommunicator.java
+++ core/src/com/example/networking/rmi/ProcessCommunicator.java
@@ -1,7 +1,27 @@
 package com.example.networking.rmi;
 
+import com.example.manager.command.Command;
+
+import java.rmi.RemoteException;
+
 /**
- *
+ * Die Schnittstelle für Javas Remote Method Invocation (RMI).
+ * Über diese Schnittstelle können die Prozesse miteinander kommunizieren.
+ * Sie wird von {@link ProcessCommunicatorImpl} implementiert.
  */
 public interface ProcessCommunicator {
+
+    /**
+     * Fügt einen {@link Command} in die Warteschlange ein.
+     * @param command Der einzufügende Befehl
+     * @throws RemoteException Wird geworfen, wenn ein Fehler bei der Kommunikation auftritt
+     */
+    void queueCommand(Command command) throws RemoteException;
+
+    /**
+     * Entfernt einen {@link Command} aus der Warteschlange.
+     * @return Der entfernte Befehl
+     * @throws RemoteException Wird geworfen, wenn ein Fehler bei der Kommunikation auftritt
+     */
+    Command dequeueCommand() throws RemoteException;
 }
--- core/src/com/example/networking/rmi/ProcessCommunicatorImpl.java
+++ core/src/com/example/networking/rmi/ProcessCommunicatorImpl.java
@@ -3,8 +3,20 @@ package com.example.networking.rmi;
 import com.example.manager.command.Command;
 
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class ProcessCommunicatorImpl {
+/**
+ * Die Implementierung von {@link ProcessCommunicator}, über welche die Prozesse miteinander kommunizieren können.
+ */
+public class ProcessCommunicatorImpl implements ProcessCommunicator {
 
-    BlockingQueue<Command> queuedCommands;
+    BlockingQueue<Command> queuedCommands = new LinkedBlockingQueue<>();
+
+    public void queueCommand(Command command) {
+        queuedCommands.add(command);
+    }
+
+    public Command dequeueCommand() {
+        return queuedCommands.poll();
+    }
 }
