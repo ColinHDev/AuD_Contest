de57d6a2f07e7b6b7e6f16725b868bcd34e7a4b7
Colin <colinheidfeld@gmail.com>
2023-11-26 04:47:04 +0100

    EndTurnCommand: Enum für die Art der Strafe (keine, Runde aussetzen oder Disqualifizierung) hinzugefügt

--- core/src/com/example/manager/command/EndTurnCommand.java
+++ core/src/com/example/manager/command/EndTurnCommand.java
@@ -10,6 +10,33 @@ import com.example.simulation.action.ActionLog;
  */
 public class EndTurnCommand extends Command {
 
+    public enum EndTurnPunishment {
+        // Der Spieler erhält keine Strafe
+        NONE,
+        // Der Spieler muss den nächsten Zug aussetzen
+        MISS_TURN,
+        // Der Spieler wird disqualifiziert und das Spiel beendet
+        DISQUALIFY
+    }
+
+    private final EndTurnPunishment punishment;
+
+    /**
+     * Erstellt einen neuen Befehl, der das Ende des aktuellen Zuges markiert.
+     * Der Spieler erhält keine Strafe.
+     */
+    public EndTurnCommand() {
+        this(EndTurnPunishment.NONE);
+    }
+
+    /**
+     * Erstellt einen neuen Befehl, der das Ende des aktuellen Zuges markiert.
+     * @param punishment Die Strafe, die der Spieler auf Basis des Zuges erhält
+     */
+    public EndTurnCommand(EndTurnPunishment punishment) {
+        this.punishment = punishment;
+    }
+
     @Override
     public ActionLog onExecute(GameCharacterController controller) {
         return null;
