3aae9c5c19021d3fae87c58049b53121804d2b07
Colin <colinheidfeld@gmail.com>
2023-11-28 02:51:07 +0100

    BotProcess: Logik des Ausführen des Bots implementiert

--- core/src/com/example/networking/BotProcess.java
+++ core/src/com/example/networking/BotProcess.java
@@ -1,13 +1,19 @@
 package com.example.networking;
 
 import com.example.manager.CompletionHandler;
+import com.example.manager.PlayerThread;
+import com.example.manager.command.Command;
 import com.example.manager.player.Player;
+import com.example.networking.data.CommunicatedInformation;
+import com.example.networking.data.GameInformation;
+import com.example.networking.data.TurnInformation;
 import com.example.networking.rmi.ProcessCommunicator;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Diese Klasse repräsentiert den Prozess, auf welchem der Bot eines Spielers ausgeführt wird.
@@ -39,6 +45,43 @@ public class BotProcess {
         }
         if (registry == null) {
             throw new RuntimeException("Could not connect to Parent Process of the main game through.");
+        }
+
+        PlayerThread playerThread = null;
+        while (true) {
+            CommunicatedInformation information;
+            try {
+                information = communicator.dequeueInformation();
+            } catch (RemoteException e) {
+                throw new RuntimeException("Could not dequeue information from the parent process.");
+            }
+            if (information instanceof GameInformation) {
+                playerThread = new PlayerThread(playerClass, ((GameInformation) information).isDebug());
+                BlockingQueue<Command> commands = playerThread.init(((GameInformation) information).state());
+                Command command;
+                do {
+                    try {
+                        command = commands.take();
+                        communicator.queueCommand(command);
+                    } catch (InterruptedException|RemoteException e) {
+                        throw new RuntimeException(e);
+                    }
+                } while (!command.endsTurn());
+            } else if (information instanceof TurnInformation) {
+                if (playerThread == null) {
+                    throw new RuntimeException("Received TurnInformation before GameInformation.");
+                }
+                BlockingQueue<Command> commands = playerThread.executeTurn(((TurnInformation) information).state());
+                Command command;
+                do {
+                    try {
+                        command = commands.take();
+                        communicator.queueCommand(command);
+                    } catch (InterruptedException | RemoteException e) {
+                        throw new RuntimeException(e);
+                    }
+                } while (!command.endsTurn());
+            }
         }
     }
 
