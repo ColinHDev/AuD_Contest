ad1070681b15583f8f377480591563690cb8b243
Colin <colinheidfeld@gmail.com>
2023-12-02 03:06:10 +0100

    MalBot und TestPolicy angepasst BotClassFilter Klasse implementiert

--- core/src/test/java/bots/MalBot.java
+++ core/src/test/java/bots/MalBot.java
@@ -12,7 +12,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.ArrayList;
 
 /**
  * MalBot is Part of a test that validates the SecurityPolicy:
@@ -25,7 +24,11 @@ public class MalBot extends Bot {
 
     private boolean first = true;
 
-    public static ArrayList<String> failedExperiments = new ArrayList<>();
+    public static final String[] ILLEGAL_IMPORTS = {
+            Manager.class.getName(),
+            "java.lang.reflect",
+            "java.net",
+    };
 
 
     @Override
@@ -45,81 +48,53 @@ public class MalBot extends Bot {
 
     @Override
     public void init(StaticGameState state) {
-        runExperiment(false, state);
+        runExperiment();
     }
 
     @Override
     public void executeTurn(StaticGameState state, Controller controller) {
         if (first) {
             first = false;
-            runExperiment(true, state);
+            runExperiment();
         }
     }
 
-    private void runExperiment(boolean inTurn, StaticGameState state) {
-        boolean caught = false;
-
+    private void runExperiment() {
         try {
             Object instance = Manager.class.getDeclaredConstructors()[0].newInstance();
-        } catch (SecurityException | IllegalAccessException e) {
-            caught = true;
+        } catch (SecurityException | IllegalAccessException ignored) {
         } catch (InstantiationException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        if (!caught) failedExperiments.add(failureMessage(1, inTurn, state));
 
-
-        caught = false;
         try {
             Field privateField = Manager.class.getDeclaredField("threadPoolExecutor");
             privateField.setAccessible(true);
 
-        } catch (SecurityException e) {
-            caught = true;
+        } catch (SecurityException ignored) {
         } catch (NoSuchFieldException e) {
             e.printStackTrace();
         }
-        if (!caught) failedExperiments.add(failureMessage(2, inTurn, state));
 
-
-        caught = false;
         try {
             File file = new File("");
             file.exists();
 
-        } catch (SecurityException e) {
-            caught = true;
+        } catch (SecurityException ignored) {
         }
-        if (!caught) failedExperiments.add(failureMessage(3, inTurn, state));
 
-        caught = false;
         Socket socket;
         try {
             socket = new Socket();
             socket.bind(new InetSocketAddress(InetAddress.getByName("localhost"), 1085));
             socket.close();
-        } catch (SecurityException e) {
-            caught = true;
+        } catch (SecurityException ignored) {
         } catch (IOException e) {
             e.printStackTrace(System.err);
         }
-        if (!caught) failedExperiments.add(failureMessage(4, inTurn, state));
-
-        caught = false;
         try {
             Object instance = Manager.getManager();
         } catch (SecurityException e) {
-            caught = true;
         }
-        if (!caught) failedExperiments.add(failureMessage(5, inTurn, state));
-
-    }
-
-    private String failureMessage(int id, boolean inTurn, StaticGameState state) {
-        return "Failure{" +
-                "id=" + id +
-                "inTurn=" + inTurn +
-                ", state=" + state +
-                '}';
     }
 }
