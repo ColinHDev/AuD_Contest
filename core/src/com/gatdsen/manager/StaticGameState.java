4025a92099ace81da85a681b7e67766ebebfe5f1
Colin <colinheidfeld@gmail.com>
2023-12-05 02:04:27 +0100

    t129 - StaticGameState, StaticPlayerState und Controller dokumentiert

--- core/src/com/gatdsen/manager/StaticGameState.java
+++ core/src/com/gatdsen/manager/StaticGameState.java
@@ -1,43 +1,79 @@
 package com.gatdsen.manager;
 
 import com.gatdsen.simulation.GameState;
+import com.gatdsen.simulation.PlayerState;
 
+/**
+ * Diese Klasse repräsentiert den aktuellen Zustand des Spiels, dementsprechend auch den aktuellen Zustand aller
+ * Spieler (siehe {@link StaticPlayerState} bzw. {@link #getMyPlayerState()}).
+ * <p>
+ * Wie es der Name bereits andeutet, ist diese Klasse statisch. Das bedeutet, dass sie sich nicht bspw. über die
+ * Dauer eines Zuges verändert.
+ */
 public final class StaticGameState {
 
     private final GameState state;
     private final int playerIndex;
-    private final StaticPlayerState[] playerStates;
+    private final StaticPlayerState[] staticPlayerStates;
 
     StaticGameState(GameState state, int playerIndex) {
         this.state = state;
         this.playerIndex = playerIndex;
-        this.playerStates = new StaticPlayerState[state.getPlayerCount()];
+        staticPlayerStates = new StaticPlayerState[state.getPlayerCount()];
+        PlayerState[] playerStates = state.getPlayerStates();
+        for (int i = 0; i < state.getPlayerCount(); i++) {
+            staticPlayerStates[i] = new StaticPlayerState(playerStates[i]);
+        }
     }
 
-    public StaticPlayerState getPlayerState() {
-        // TODO
-        // return ;
-        return null;
+    /**
+     * @return Gibt den PlayerState des aktuellen Spielers zurück
+     */
+    public StaticPlayerState getMyPlayerState() {
+        return staticPlayerStates[playerIndex];
     }
 
+    /**
+     * @return Gibt den PlayerState des Gegners zurück
+     */
     public StaticPlayerState getEnemyPlayerState() {
-        // TODO
-        // return ;
-        return null;
+        return staticPlayerStates[(playerIndex + 1) % state.getPlayerCount()];
     }
 
+    /**
+     * @return Gibt die aktuelle Runde zurück.
+     * <p>
+     * Dieser Wert ist nicht nur nützlich, um zu überprüfen, wie viele Runden das Spiel bereits andauert, sondern
+     * ebenfalls um zu überprüfen, ob man eine Runde aussetzen musste, da in diesem Fall die {@code executeTurn()} Methode des
+     * Spielers nicht aufgerufen wird.
+     */
     public int getTurn() {
         return state.getTurn();
     }
 
+    /**
+     * @return Gibt die Anzahl der Spieler zurück, die am Spiel teilnehmen.
+     * <p>
+     * Im Normalfall sind dies zwei Spieler.
+     */
     public int getPlayerCount() {
         return state.getPlayerCount();
     }
 
+    /**
+     * @return Gibt die Breite des Spielfeldes zurück.
+     * <p>
+     * Diese Methode kann bspw. in Verbindung mit {@link StaticPlayerState#getBoard()} verwendet werden.
+     */
     public int getBoardSizeX() {
         return state.getBoardSizeX();
     }
 
+    /**
+     * @return Gibt die Höhe des Spielfeldes zurück.
+     * <p>
+     * Diese Methode kann bspw. in Verbindung mit {@link StaticPlayerState#getBoard()} verwendet werden.
+     */
     public int getBoardSizeY() {
         return state.getBoardSizeY();
     }
--- core/src/com/gatdsen/manager/StaticPlayerState.java
+++ core/src/com/gatdsen/manager/StaticPlayerState.java
@@ -2,7 +2,16 @@ package com.gatdsen.manager;
 
 import com.gatdsen.simulation.PlayerState;
 import com.gatdsen.simulation.Tile;
+import com.gatdsen.simulation.PathTile;
+import com.gatdsen.simulation.Tower;
 
+/**
+ * Diese Klasse repräsentiert den aktuellen Zustand eines Spielers.
+ * Dies umfasst sowohl Attribute, wie dessen Leben und Geld, als auch dessen Map.
+ * <p>
+ * Wie es der Name bereits andeutet, ist diese Klasse statisch. Das bedeutet, dass sie sich nicht bspw. über die
+ * Dauer eines Zuges verändert.
+ */
 public final class StaticPlayerState {
 
     private final PlayerState state;
@@ -11,23 +20,40 @@ public final class StaticPlayerState {
         this.state = state;
     }
 
-    PlayerState getPlayerState() {
-        return state;
+    /**
+     * @return Die Id des Spielers, welche verwendet wird, um den Spieler bspw. aus einer Liste
+     * von {@link StaticPlayerState}s zu identifizieren.
+     */
+    public int getId() {
+        return state.getIndex();
     }
 
+    /**
+     * @return Die Anzahl der verbleibenden Leben des Spielers
+     */
     public int getHealth() {
         return state.getHealth();
     }
 
+    /**
+     * @return Die Anzahl des angesparten Geldes des Spielers
+     */
     public int getMoney() {
-        // TODO
-        // return state.getMoney();
-        return 0;
+        return state.getMoney();
     }
 
-    public Tile[][] getMap() {
-        // TODO
-        //return state.getMap();
-        return null;
+    /**
+     * Gibt einen zwei-dimensionalen Array von {@link Tile}s zurück, indexiert anhand ihrer x- und y-Koordinaten.
+     * <p> - {@link PathTile} Instanzen symbolisieren dabei einen Pfad.
+     * <p> - {@link Tower} Instanzen symbolisieren einen Tower.
+     * <p> - {@code null} symbolisiert ein leeres Feld, an welchem sich weder ein Pfad noch ein Tower befindet. An diesen
+     * Feldern können neue Türme platziert werden.
+     * <p>
+     * Das Array ist über die x- und y-Koordinate des {@link Tile}s indexiert.
+     * {@code map[x][y]} gibt also das {@link Tile} an der Position {@code (x, y)} zurück.
+     * @return Die Map des Spielers
+     */
+    public Tile[][] getBoard() {
+        return state.getBoard();
     }
 }
