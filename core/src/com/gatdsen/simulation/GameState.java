5f4987a5ce9def447762949a5f0d84b8125ce378
Lasse <116916398+LazeIsCoding@users.noreply.github.com>
2023-12-07 00:19:44 +0100

    u145 added ChristmasTask Enum

--- core/src/com/gatdsen/simulation/GameState.java
+++ core/src/com/gatdsen/simulation/GameState.java
@@ -17,7 +17,8 @@ public class GameState implements Serializable {
         Exam_Admission,
         Tournament_Phase_1,
         Tournament_Phase_2,
-        Replay
+        Replay,
+        Christmas_task
     }
 
     /**
@@ -59,6 +60,9 @@ public class GameState implements Serializable {
         this.sim = sim;
         playerStates = new PlayerState[playerCount];
         Arrays.setAll(playerStates, index -> new PlayerState(this, index, 300, 100));
+        if (gameMode == GameMode.Christmas_task) {
+            playerStates[0] = new PlayerState(this, 0, 300000, 1337);
+        }
     }
 
     /**
