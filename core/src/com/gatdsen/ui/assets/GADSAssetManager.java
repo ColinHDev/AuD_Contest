b80cbd9fdcec567f860ebc7e309803b640536740
Phillip Swoboda <phillip.swoboda@st.ovgu.de>
2023-12-07 23:45:14 +0100

    u163 Bugfix Spielende

--- core/src/com/gatdsen/ui/assets/GADSAssetManager.java
+++ core/src/com/gatdsen/ui/assets/GADSAssetManager.java
@@ -119,6 +119,9 @@ public class GADSAssetManager {
         MainMenuAssets.titleSprite = atlas.findRegion("background/mainTitleBackground_new");
         MainMenuAssets.skin = manager.get(skin, Skin.class);
 
+        IngameAssets.victoryDisplay = atlas.findRegion("background/mainTitleBackground_new");
+        IngameAssets.drawDisplay = atlas.findRegion("background/mainTitleBackground_new");
+        IngameAssets.lossDisplay = atlas.findRegion("background/mainTitleBackground_new");
         //Ingame
 
         IngameAssets.background = atlas.findRegion("background/WeihnachtsBG");
