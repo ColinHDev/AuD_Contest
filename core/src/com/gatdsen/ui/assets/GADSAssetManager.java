3e6746bf09f1a1ea74d9a64306078a83d2143a29
ldamitz <lukas.damitz@ovgu.de>
2023-12-08 00:30:16 +0100

    Tiles hinzugefügt

--- core/src/com/gatdsen/ui/assets/GADSAssetManager.java
+++ core/src/com/gatdsen/ui/assets/GADSAssetManager.java
@@ -125,15 +125,15 @@ public class GADSAssetManager {
 
                 // Path Tiles siehe TileMap
                 //0: Spawn nach oben
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/start_tile"),
                 //1: Spawn nach rechts
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/start_tile"),
                 //2: Spawn nach unten
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/start_tile"),
                 //3: Spawn nach rechts
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/start_tile"),
                 //4: Ziel nach unten
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/end_tile"),
                 //5: Ecke rechts Oben
                 atlas.findRegion("Tileset/path_right_up_tile"),
                 //6: vertikale Gerade
@@ -141,23 +141,23 @@ public class GADSAssetManager {
                 //7: Ecke links Oben
                 atlas.findRegion("Tileset/path_left_up_tile"),
                 //8: Ziel nach links
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/end_tile"),
                 //9: Ecke rechts Unten
                 atlas.findRegion("Tileset/path_right_down_tile"),
                 //10: horizontale Gerade
                 atlas.findRegion("Tileset/path_horizontal_tile"),
                 //11: Ziel nach oben
-                atlas.findRegion("Tileset/water_tile"),
+                atlas.findRegion("Tileset/end_tile"),
                 //12: Ecke links Unten
                 atlas.findRegion("Tileset/path_left_down_tile"),
                 //13: Ziel nach rechts
-                atlas.findRegion("Tileset/water_tile")
+                atlas.findRegion("Tileset/end_tile")
         };
 
         IngameAssets.gameCharacterAnimations = new AtlasAnimation[GameCharacterAnimationType.values().length];
 
         //IngameAssets.gameCharacterAnimations[GameCharacterAnimationType.ANIMATION_TYPE_IDLE.ordinal()] = new AtlasAnimation(1 / 10f, atlas.findRegions("mageCat_idle_down/mageCat_idle_down_0"), Animation.PlayMode.LOOP);
-        IngameAssets.gameCharacterAnimations[GameCharacterAnimationType.ANIMATION_TYPE_IDLE.ordinal()] = new AtlasAnimation(1 / 10f, atlas.findRegions("idle/mage_Cat_idle_down"), Animation.PlayMode.LOOP);
+        IngameAssets.gameCharacterAnimations[GameCharacterAnimationType.ANIMATION_TYPE_IDLE.ordinal()] = new AtlasAnimation(1 / 10f, atlas.findRegions("mageCat_idle_down/mage_Cat_idle_down"), Animation.PlayMode.LOOP);
 
         IngameAssets.gameCharacterAnimations[GameCharacterAnimationType.ANIMATION_TYPE_WALKING.ordinal()] = new AtlasAnimation(1 / 10f, atlas.findRegions("enemy/bigMouse_idle_left"), Animation.PlayMode.LOOP);
 
