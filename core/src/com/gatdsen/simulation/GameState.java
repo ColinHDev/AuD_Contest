58fa5c4ab113931d6c8ead1c07de1c43010c41ea
Colin <colinheidfeld@gmail.com>
2023-12-08 00:09:35 +0100

    GameState: Index gefixt

--- core/src/com/gatdsen/simulation/GameState.java
+++ core/src/com/gatdsen/simulation/GameState.java
@@ -61,7 +61,7 @@ public class GameState implements Serializable {
         playerStates = new PlayerState[playerCount];
         Arrays.setAll(playerStates, index -> new PlayerState(this, index, 300, 100));
         if (gameMode == GameMode.Christmas_Task) {
-            playerStates[1] = new PlayerState(this, 0, 300000, 0);
+            playerStates[1] = new PlayerState(this, 1, 300000, 0);
         }
     }
 
